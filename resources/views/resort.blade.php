d6e87a4cc0d2b99853e9774761896690bb3f0be7
Gavin Cheng <gavincheng01@gmail.com>
2017-11-29 10:59:26 -0500

    resort detials from database are listed

--- resources/views/resort.blade.php
+++ resources/views/resort.blade.php
@@ -2,9 +2,26 @@
 
 @section('content')
 
+<div id="resort">
 <link href="/css/style.css" rel="stylesheet">
 <h1>{{ $resort->name }}</h1>
+<h5>{{ $resort->location }}</h3>
 
+<ul>
+	<li><span class="attribute">Open Status:</span> 
+		@if ( $resort->open === true) 
+		<span style="color:green;">Open </span>
+		@else <span style="color:red;">Closed </span>
+		@endif
+	</li>
+	<li><span class="attribute">Condition:</span> {{ $resort->conditions }}</li>
+	<li><span class="attribute">Percentage Open:</span>  {{ $resort->pct_open }} %</li>
+	<li><span class="attribute">Lifts Open:</span>  {{ $resort->lifts_open }}</li>
+	<li><span class="attribute">Last Snowfall:</span>  {{ $resort->last_snow }}</li>
+	<li><span class="attribute">New Snow last 24hrs(in):</span>  {{ $resort->new_snow_in }}"</li>
+	<li><span class="attribute">Upper Snow(in):</span>  {{ $resort->upper_snow }}</li>
+	<li><span class="attribute">Upper Snow(in):</span>  {{ $resort->lower_snow }}</li>
+</ul>
 
 
 
