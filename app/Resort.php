b5afe712bb6d9f605cbd019d8d0b9798d9c77690
Gavin Cheng <gavincheng01@gmail.com>
2017-11-29 11:18:30 -0500

    made pivot table between resorts and users

--- app/Resort.php
+++ app/Resort.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Resort extends Model
-{
-    //
+class Resort extends Model {
+
+
+    public function users() {
+
+        return $this->belongsToMany('App\User', 'resort_user', 'resorts_id', 'user_id');
+
+    }
 }
