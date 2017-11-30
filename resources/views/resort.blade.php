267ce50ac26b08e987d3ba68cc975f8170f6dc47
Gavin Cheng <gavincheng01@gmail.com>
2017-11-30 10:28:52 -0500

    changed resort blade to view attributes as a table instead of a table

--- resources/views/resort.blade.php
+++ resources/views/resort.blade.php
@@ -7,22 +7,50 @@
 <h1 style="color:#30A9DE">{{ $resort->name }}</h1>
 <h5>{{ $resort->location }}</h3>
 
-<ul>
-	<li><span class="attribute">Open Status:</span> 
-		@if ( $resort->open === true) 
-		<span style="color:green;">Open </span>
-		@else <span style="color:red;">Closed </span>
-		@endif
-	</li>
-	<li><span class="attribute">Condition:</span> {{ $resort->conditions }}</li>
-	<li><span class="attribute">Percentage Open:</span>  {{ $resort->pct_open }} %</li>
-	<li><span class="attribute">Lifts Open:</span>  {{ $resort->lifts_open }}</li>
-	<li><span class="attribute">Last Snowfall:</span>  {{ $resort->last_snow }}</li>
-	<li><span class="attribute">New Snow last 24hrs(in):</span>  {{ $resort->new_snow_in }}"</li>
-	<li><span class="attribute">Upper Snow(in):</span>  {{ $resort->upper_snow }}</li>
-	<li><span class="attribute">Upper Snow(in):</span>  {{ $resort->lower_snow }}</li>
-	<li><a href="{{ $resort->website }}"><span class="attribute">Visit Website!</span></a></li>
-</ul>
+
+<div class="tableDiv">
+	<table class="table resortTable">
+
+	    </thead>
+	    <tbody>
+	      	<tr>
+		        <td><span class="attribute">Open Status:</span> 
+			@if ( $resort->open === true) 
+			</td>
+		        <td><span style="color:green;">Open </span>
+			@else <span style="color:red;">Closed </span>
+			@endif</td>
+	      	</tr>
+	      	<tr>
+		        <td><span class="attribute">Condition:</span></td>
+		        <td>{{ $resort->conditions }}</td>
+	      	</tr>
+	      	<tr>
+		        <td><span class="attribute">Percentage Open:</span></td>
+		        <td>{{ $resort->pct_open }}%</td>
+	      	</tr>
+	      	<tr>
+		        <td><span class="attribute">Lifts Open:</span></td>
+		        <td>{{ $resort->lifts_open }}</td>
+	      	</tr>
+	      	<tr>
+		        <td><span class="attribute">Last Snowfall:</span></td>
+		        <td>{{ $resort->last_snow }}</td>
+	      	</tr>
+	      	<tr>
+		        <td><span class="attribute">New Snow last 24hrs(in)</span></td>
+		        <td>{{ $resort->upper_snow }}</td>
+	      	</tr>
+	      	<tr>
+		        <td><span class="attribute">Upper Snow(in)</span></td>
+		        <td>{{ $resort->lower_snow }}</td>
+	      	</tr>
+
+	    </tbody>
+	</table>
+
+
+</div>
 
 
 
